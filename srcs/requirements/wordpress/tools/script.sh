59a2b2c4ea8cf76c4ab8feb48e724caf4b6e8c8a
snakajim <snakajim@student.42tokyo.jp>
2026-01-09 17:42:41 +0900

    Done at MacBook

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,15 +2,17 @@
 
 set -e
 
-if [ ! -f "/var/www/html/wp-config.php" ]; then
+cd /var/www/html
+if [ ! -f "/var/www/html/index.php" ]; then
 	echo "Downloading WordPress"
-	cd /var/www/html
 	wp core download --allow-root
-	echo "Waiting for MariaDB"
-	while ! mariadb-admin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" --silent; do
-		sleep 1
-	done
+fi
 
+echo "Waiting for MariaDB"
+while ! mariadb-admin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASS" --silent; do
+	sleep 1
+done
+if [ ! -f "/var/www/html/wp-config.php" ]; then
 	wp config create \
 		--dbname=$DB_NAME \
 		--dbuser=$DB_USER \
@@ -24,8 +26,13 @@ if [ ! -f "/var/www/html/wp-config.php" ]; then
 		--admin_password=$WP_ADMIN_PASS \
 		--admin_email=$WP_ADMIN_EMAIL \
 		--allow-root
+	wp user create \
+		yehara \
+		yehara@gmail.com \
+		--role=administrator \
+		--allow-root
+
 	echo "WordPress setup completed"
 fi
-echo "Starting PHP-FPM 8.4"
-
-exec /usr/sbin/php-fpm84 -F
+echo "Starting PHP-FPM 8.3"
+exec /usr/sbin/php-fpm83 -F
